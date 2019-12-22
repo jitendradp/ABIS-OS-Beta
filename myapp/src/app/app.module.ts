046a5371d65e17c6f7b9f5435282e599e3e05e67
dbidev <development@dbi-solutions.de>
2019-12-22 13:02:36 +0100

    finished basic layout

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -4,14 +4,21 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule, Routes} from "@angular/router";
-import {LoginComponent} from './login/login.component';
-import {SignupComponent} from './signup/signup.component';
+import {LoginComponent} from './pages/login/login.component';
+import {SignupComponent} from './pages/signup/signup.component';
+import {MatGridListModule, MatToolbarModule} from "@angular/material";
+import {HeaderComponent} from './header/header.component';
+import {BodyComponent} from './body/body.component';
+import {FooterComponent} from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +27,9 @@ import {SignupComponent} from './signup/signup.component';
     RouterModule.forRoot([
       {path: 'login', component: LoginComponent},
       {path: 'signup', component: SignupComponent}
-    ])
+    ]),
+    MatToolbarModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
