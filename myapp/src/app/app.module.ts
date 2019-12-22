db20d75b8aab4bb8c608d11e02c7cd2276c40d4f
dbidev <development@dbi-solutions.de>
2019-12-22 17:25:22 +0100

    intermediate

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -5,10 +5,10 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-  MatButtonModule,
+  MatButtonModule, MatFormFieldControl, MatFormFieldModule,
   MatGridListModule,
-  MatIconModule, MatListModule,
-  MatMenuModule,
+  MatIconModule, MatInputModule, MatListModule,
+  MatMenuModule, MatOptionModule, MatSelectModule,
   MatSidenavModule,
   MatToolbarModule
 } from "@angular/material";
@@ -18,7 +18,7 @@ import {ContentComponent} from './content/content.component';
 import {ButtonComponent} from './button/button.component';
 import {RegisterComponent} from './pages/register/register.component';
 import {SigninComponent} from './pages/signin/signin.component';
-import { GobackComponent } from './goback/goback.component';
+import {GobackComponent} from './goback/goback.component';
 
 const appRoutes: Routes = [
   {path: 'access', component: AccessComponent},
@@ -51,7 +51,11 @@ const appRoutes: Routes = [
     MatMenuModule,
     MatIconModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
