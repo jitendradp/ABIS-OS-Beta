e35f35bf42e8817c3eb4d0bc3d069ad5ee7b865a
dbidev <development@dbi-solutions.de>
2019-12-22 17:39:20 +0100

    test

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-  MatButtonModule, MatFormFieldControl, MatFormFieldModule,
+  MatButtonModule, MatFormFieldModule,
   MatGridListModule,
   MatIconModule, MatInputModule, MatListModule,
   MatMenuModule, MatOptionModule, MatSelectModule,
