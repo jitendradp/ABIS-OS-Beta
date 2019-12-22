51919a9edba80e5f8303251c704326f743f0b9c2
dbidev <development@dbi-solutions.de>
2019-12-22 21:36:37 +0100

    f** mat header overwrite css s***

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  picture: string = 'http://i.imgur.com/74sByqd.jpg';
+
+  @Input()
+  cover: string = 'https://htmlcolorcodes.com/assets/images/html-color-codes-color-tutorials-hero-00e10b1f.jpg';
+
+  @Input()
+  name: string = 'David Chang';
+
+  @Input()
+  slogan: string = 'To be or not to be, this is my awesome motto!';
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
