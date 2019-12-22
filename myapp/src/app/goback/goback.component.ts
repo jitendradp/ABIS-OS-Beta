5481f4040dcffc6014deb7b30371e0747b706262
dbidev <development@dbi-solutions.de>
2019-12-22 17:12:10 +0100

    added links

---
+++ myapp/src/app/goback/goback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goback',
+  templateUrl: './goback.component.html',
+  styleUrls: ['./goback.component.css']
+})
+export class GobackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
