68de8da7065951f5e941bb24867e88ff648e06b8
dbidev <development@dbi-solutions.de>
2019-12-28 17:07:24 +0100

    added the group and member list to sidebar

--- frontend/src/app/components/menu/menu.component.ts
+++ frontend/src/app/components/menu/menu.component.ts
@@ -2,6 +2,11 @@ import {Component, Input} from '@angular/core';
 import {WorkspaceService} from "../../services/workspace.service";
 import {ProfileService} from "../../services/profile.service";
 
+export interface Section {
+  name: string;
+  updated: Date;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -26,4 +31,29 @@ export class MenuComponent {
     this.profile = this._profileService.getProfileInformation();
     this.workspace = this._workspaceService.getWorkspaceInformation();
   }
+
+  channels: Section[] = [
+    {
+      name: 'Chaos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'General',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Marketing',
+      updated: new Date('1/28/16'),
+    }
+  ];
+  members: Section[] = [
+    {
+      name: 'Daniel Janz',
+      updated: new Date('2/20/16'),
+    },
+    {
+      name: 'Moritz Bönke',
+      updated: new Date('1/18/16'),
+    }
+  ];
 }
