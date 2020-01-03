0fbe2e1e15f48850409a5ddab5e4586fd1c966c6
dbidev <development@dbi-solutions.de>
2020-01-04 00:26:48 +0100

    added fx hide very useful

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-  MatBadgeModule, MatBottomSheetModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
   MatButtonModule,
   MatButtonToggleModule,
   MatCardModule,
