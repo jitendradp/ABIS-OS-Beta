f797288a1f6e02c686bba654b226273a6672bbcb
dbidev <development@dbi-solutions.de>
2020-01-06 04:49:22 +0100

    designed draft of store card

--- frontend/src/app/pages/system/access/access.component.ts
+++ frontend/src/app/pages/system/access/access.component.ts
@@ -7,7 +7,7 @@ import {AccountService} from "../../../services/account.service";
   styleUrls: ['./access.component.css']
 })
 export class AccessComponent {
-  constructor(protected _accountService: AccountService) {
+  constructor(public _accountService: AccountService) {
   }
 
   @Input()
