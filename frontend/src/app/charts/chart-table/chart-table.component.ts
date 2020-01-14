82c2b4509a1ea552b56cf9793b49dddc322d5620
dbidev <development@dbi-solutions.de>
2020-01-14 20:19:52 +0100

    added auto resize of the columns to the ag-grid

--- frontend/src/app/charts/chart-table/chart-table.component.ts
+++ frontend/src/app/charts/chart-table/chart-table.component.ts
@@ -1,5 +1,8 @@
 import {Component} from "@angular/core";
 
+
+// Find more properties for the ag-grid here: https://www.ag-grid.com/javascript-grid-column-properties/
+
 @Component({
   selector: 'app-chart-table',
   templateUrl: './chart-table.component.html',
@@ -7,17 +10,46 @@ import {Component} from "@angular/core";
 })
 export class ChartTableComponent {
   columnDefs = [
-    {headerName: 'Coin', field: 'coin', sortable: true, filter: true, width: 100},
-    {headerName: 'Holdings', field: 'holdings', sortable: true, filter: true, width: 100},
-    {headerName: 'Price', field: 'price', sortable: true, filter: true, width: 100},
+    {headerName: 'Coin', field: 'coin', sortable: true, filter: true},
+    {headerName: 'Holdings', field: 'holdings', sortable: true, filter: false},
+    {headerName: 'Price', field: 'price', sortable: true, filter: false},
   ];
 
   rowData = [
     {coin: 'BTC', holdings: 83.343, price: 19.23},
     {coin: 'ETH', holdings: 793.34, price: 23.34},
-    {coin: 'LTC', holdings: 893.34, price: 93.43}
+    {coin: 'LTC', holdings: 893.34, price: 93.43},
+    {coin: 'BTC', holdings: 83.343, price: 19.23},
+    {coin: 'ETH', holdings: 793.34, price: 23.34},
+    {coin: 'LTC', holdings: 893.34, price: 93.43},
+    {coin: 'BTC', holdings: 83.343, price: 19.23},
+    {coin: 'ETH', holdings: 793.34, price: 23.34},
+    {coin: 'LTC', holdings: 893.34, price: 93.43},
+    {coin: 'BTC', holdings: 83.343, price: 19.23},
+    {coin: 'ETH', holdings: 793.34, price: 23.34},
+    {coin: 'LTC', holdings: 893.34, price: 93.43},
+    {coin: 'BTC', holdings: 83.343, price: 19.23},
+    {coin: 'ETH', holdings: 793.34, price: 23.34},
+    {coin: 'LTC', holdings: 893.34, price: 93.43},
+    {coin: 'BTC', holdings: 83.343, price: 19.23},
+    {coin: 'ETH', holdings: 793.34, price: 23.34},
+    {coin: 'LTC', holdings: 893.34, price: 93.43},
+    {coin: 'BTC', holdings: 83.343, price: 19.23},
+    {coin: 'ETH', holdings: 793.34, price: 23.34},
+    {coin: 'LTC', holdings: 893.34, price: 93.43},
+    {coin: 'BTC', holdings: 83.343, price: 19.23},
+    {coin: 'ETH', holdings: 793.34, price: 23.34},
+    {coin: 'LTC', holdings: 893.34, price: 93.43},
+    {coin: 'BTC', holdings: 83.343, price: 19.23},
+    {coin: 'ETH', holdings: 793.34, price: 23.34},
+    {coin: 'LTC', holdings: 893.34, price: 93.43},
+    {coin: 'BTC', holdings: 83.343, price: 19.23},
+    {coin: 'ETH', holdings: 793.34, price: 23.34},
+    {coin: 'LTC', holdings: 893.34, price: 93.43},
   ];
 
+  onFirstDataRendered(params) {
+    params.api.sizeColumnsToFit();
+  }
+
 }
-
-
