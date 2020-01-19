94294efae675a9fce647fe74323ee392afabfe59
dbidev <development@dbi-solutions.de>
2020-01-19 13:16:54 +0100

    reduced the editor room form to two steps only

--- frontend/src/app/editors/editor-room/editor-room.component.ts
+++ frontend/src/app/editors/editor-room/editor-room.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
@@ -7,6 +7,9 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./editor-room.component.css']
 })
 export class EditorRoomComponent {
+
+  @Input()
+  isPublic: boolean;
 
   isLinear = true;
   firstFormGroup: FormGroup;
