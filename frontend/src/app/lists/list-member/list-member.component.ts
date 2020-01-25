eaaef005ab4048c92aef0bff59df752345ebbe65
dbidev <development@dbi-solutions.de>
2020-01-25 03:35:44 +0100

    added some membership list as small preview on the chat content header section

--- frontend/src/app/lists/list-member/list-member.component.ts
+++ frontend/src/app/lists/list-member/list-member.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+
+export interface Profile {
+  name: string;
+  lastSeen: string;
+  picture: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-list-member',
   templateUrl: './list-member.component.html',
   styleUrls: ['./list-member.component.css']
 })
-export class ListMemberComponent implements OnInit {
 
-  constructor() { }
+export class ListMemberComponent {
 
-  ngOnInit() {
-  }
+  members: Profile [] = [
+    {
+      name: 'John Doe',
+      lastSeen: 'Friday, 2020-01-01 9:15 AM',
+      picture: 'https://www.fillmurray.com/50/50',
+      status: 'offline',
+    },
+    {
+      name: 'John Doe',
+      lastSeen: 'Friday, 2020-01-01 9:15 AM',
+      picture: 'https://www.fillmurray.com/100/100',
+      status: 'online',
+    },
+    {
+      name: 'John Doe',
+      lastSeen: 'Friday, 2020-01-01 9:15 AM',
+      picture: 'https://www.fillmurray.com/200/200',
+      status: 'online',
+    },
+    {
+      name: 'John Doe',
+      lastSeen: 'Friday, 2020-01-01 9:15 AM',
+      picture: 'https://www.fillmurray.com/150/150',
+      status: 'away',
+    },
+  ];
 
 }
